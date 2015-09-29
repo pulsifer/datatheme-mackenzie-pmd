fc3916e5476edf540f7d7894e5038d4f486ec693
Boris Cosic <boris.cosic@tesera.com>
2015-09-29 17:19:44 -0600

    Fix theme.

---
+++ build.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+'use strict';
+// usage: node ./build.js <datatheme_name>
+// example: node ./build.js dev.afgo.pgyi
+
+var fs = require('fs');
+var JSONStream = require('JSONStream');
+var es = require('event-stream');
+
+fs.createReadStream('datatheme.json')
+    .pipe(JSONStream.parse())
+    .pipe(es.map(function (theme, callback) {
+        var resources = theme.resources.map(function (r) {
+            r.schema = JSON.parse(fs.readFileSync(r.schema, 'utf8'));
+            if(process.argv[2]) r.validator = r.validator.replace('mackenzie.pmd', process.argv[2]);
+            return r;
+        });
+        delete theme.resources;
+        var datapackage = {
+            name: 'mackenzie-pmd',
+            theme: theme,
+            resources: resources
+        };
+        callback(null, JSON.stringify(datapackage));
+    }))
+    .pipe(process.stdout);
